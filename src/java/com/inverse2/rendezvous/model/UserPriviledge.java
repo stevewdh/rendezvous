b314a29c3f1a507ba10d9c94edc006b59d5bd062
Stephen Harding <steve.wdh@gmail.com>
2009-04-20 15:11:59 +0100

    Misse adding this file to the last commit.

---
+++ src/java/com/inverse2/rendezvous/model/UserPriviledge.java
@@ -0,0 +1,47 @@
+package com.inverse2.rendezvous.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class UserPriviledge {
+	
+	private Integer userPriviledgeId;
+	private String  priviledgeCode;
+	private String  priviledgeDescription;
+	private String  active;
+	
+	public Integer getUserPriviledgeId() {
+		return userPriviledgeId;
+	}
+
+	public void setUserPriviledgeId(Integer userPriviledgeId) {
+		this.userPriviledgeId = userPriviledgeId;
+	}
+
+	public String getPriviledgeCode() {
+		return priviledgeCode;
+	}
+	
+	public void setPriviledgeCode(String priviledgeCode) {
+		this.priviledgeCode = priviledgeCode;
+	}
+	
+	public String getPriviledgeDescription() {
+		return priviledgeDescription;
+	}
+	
+	public void setPriviledgeDescription(String priviledgeDescription) {
+		this.priviledgeDescription = priviledgeDescription;
+	}
+	
+	public String getActive() {
+		return active;
+	}
+	
+	public void setActive(String active) {
+		this.active = active;
+	}
+	
+	
+}
